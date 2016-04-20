93f540d93788a88f0d97fd291a1e85fa4b8a8de4
David C. Norris <david@dnc-llc.com>
2016-04-19 21:54:35 -0700

    Sketch visualization for main 5x10x10 bootstrap

---
+++ outputs/bootstrap.R
@@ -0,0 +1,41 @@
+## Graphical descriptions of MTO bootstrap analyses
+
+library(rms)
+library(lattice)
+
+orci <- read.delim("orci.tab")
+
+## 1. Subset to select just the integer imods 1..10,
+##    thereby excluding the alt-specification outputs
+orci <- subset(orci, imod %in% as.character(1:10) & voucher=='RA_GRP_EXP')
+
+## 2. Convert mi_seed and pr_seed to *factors*, and imod back to integer!
+orci <- upData(orci,
+               pr_seed = factor(pr_seed),
+               mi_seed = factor(mi_seed),
+               imod = as.integer(as.character(imod)))
+
+## 2. Plot imod-bootstrapped OR, CI by pr_seed, mi_seed
+p <- xYplot(Cbind(log_or, log_lowor, log_upor) ~ imod | mi_seed * pr_seed, data=orci,
+            layout=c(10,2), ylim=log(c(0.8,10)),
+            ylab="Estimated Experimental Voucher Effect (OR [95% CI])",
+            xlab="PTSD Imputation Model Resample No.",
+            scales=list(
+                y=list(
+                    at=log(1:9),
+                    labels=as.character(1:9)
+                )
+            ),
+            par.strip.text=list(cex=0.6),
+            panel=function(...){
+                panel.refline(h=0.0)
+                panel.xYplot(...)
+            })
+
+pdf("bootstrap.pdf")
+print(p)
+dev.off()
+
+png("bootstrap.png", width=800, height=600)
+print(p)
+dev.off()
