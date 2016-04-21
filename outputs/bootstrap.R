dc17f4994fe37291f0a34080d7f3d745aa18702b
David C. Norris <david@dnc-llc.com>
2016-04-21 15:11:11 -0700

    Add alt-bootstrap graphic placeholder

--- outputs/bootstrap.R
+++ outputs/bootstrap.R
@@ -5,20 +5,31 @@ library(lattice)
 
 orci <- read.delim("orci.tab")
 
-## 1. Subset to select just the integer imods 1..10,
-##    thereby excluding the alt-specification outputs
+## 1. Convert mi_seed and pr_seed to *factors*
+orci <- upData(orci
+             , pr_seed = factor(pr_seed)
+             , mi_seed = factor(mi_seed)
+               )
+
+## 2. Subset to select just the integer imods 1..10 into 'orci',
+##    and the alt-specification results into 'alt.orci'
+alt.orci <- subset(orci, !(imod %in% as.character(0:10)) & voucher=='RA_GRP_EXP')
 orci <- subset(orci, imod %in% as.character(1:10) & voucher=='RA_GRP_EXP')
 
-## 2. Convert mi_seed and pr_seed to *factors*, and imod back to integer!
-orci <- upData(orci,
-               pr_seed = factor(pr_seed),
-               mi_seed = factor(mi_seed),
-               imod = as.integer(as.character(imod)),
-               jama = pr_seed==1234567 & mi_seed==524232 & imod==1)
+## 2. Convert imod back to integer in 'orci', and introduce
+##    suitable 'JAMA repro' indicators in each subset:
+orci <- upData(orci
+             , imod = as.integer(as.character(imod))
+             , jama = pr_seed==1234567 & mi_seed==524232 & imod==1
+               )
+
+alt.orci <- upData(alt.orci
+                 , jama = (pr_seed==1234567 & mi_seed==524232 & imod=='A1R1S99')
+                   )
 
 ## 2. Plot imod-bootstrapped OR, CI by pr_seed, mi_seed
 p <- xYplot(Cbind(log_or, log_lowor, log_upor) ~ imod | mi_seed * pr_seed, group=jama,
-            data=orci, layout=c(10,2), ylim=log(c(0.8,10)),
+            data=orci, layout=c(10,2), aspect=2.0, ylim=log(c(0.8,10)),
             ylab="Estimated Experimental Voucher Effect (OR [95% CI])",
             xlab="PTSD Imputation Model Resample No.",
             scales=list(
@@ -40,6 +51,26 @@ pdf("bootstrap.pdf")
 print(p)
 dev.off()
 
-png("bootstrap.png", width=800, height=600)
-print(p)
+q <- dotplot(Cbind(log_or, log_lowor, log_upor) ~ imod | mi_seed * pr_seed, group=jama,
+             data=alt.orci,
+             ##layout=c(10,2),
+             ##aspect=2.0,
+             ylim=log(c(0.8,10)),
+             ylab="Estimated Experimental Voucher Effect (OR [95% CI])",
+             xlab="PTSD Imputation Model Specification",
+             scales=list(
+                 x=list(rot=60),
+                 y=list(
+                     at=log(1:9),
+                     labels=as.character(1:9)
+                 )
+             ),
+             par.strip.text=list(cex=0.6),
+             panel=function(...){
+                 panel.refline(h=0.0)
+                 panel.xYplot(...)
+             })
+
+pdf("alt-bootstrap.pdf")
+print(q)
 dev.off()
